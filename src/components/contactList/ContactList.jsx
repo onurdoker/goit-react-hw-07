07c381a56a252a35414e5ccfbca7d0b1634e8700
Onur DÖKER <odoker@msn.com>
2025-10-09 23:56:16 +0300

    fix: update contact list filtering logic

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,39 +1,15 @@
 import { useSelector } from "react-redux";
-
-import { selectContacts } from "../../redux/contactsSlice.js";
-import { selectNameFilter } from "../../redux/filtersSlice.js";
-import Contacts from "../contact/Contacts.jsx";
-
-import styles from "./ContactList.module.css";
+import { selectFilteredContacts } from "./redux/contactsSlice";
 
 const ContactList = () => {
-  
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  
-  const filteredContacts = contacts
-      .filter((person) => person.name.toLowerCase()
-                                .startsWith(filter))
-      .sort((a,
-             b) => a.name.localeCompare(b.name));
-  
-  
+  const filteredContacts = useSelector(selectFilteredContacts);
   return (
-      <div className={styles.body}>
-        <ul>
-          {filteredContacts.length === 0
-              ? (<p>There are no available contacts!</p>)
-              : (filteredContacts.map((person) => (<div className={styles.card}>
-                                        <Contacts
-                                            key={person.id}
-                                            contact={person}
-                                        />
-                                      </div>),
-              ))}
-        </ul>
-      </div>
+      <ul>
+        {filteredContacts.map((contact) => (
+            <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ul>
   );
-  
 };
 
 export default ContactList;
