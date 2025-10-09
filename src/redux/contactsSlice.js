7d36b0e4e55de503dbb244117267e17e80c053ca
Onur DÖKER <odoker@msn.com>
2025-10-09 21:10:00 +0300

    Refactor `store.js` and update `contactsSlice.js` to use async thunks for contact operations

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,61 +1,88 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const contactsSlice = createSlice({
-                                    name: "contacts",
-                                    initialState: {
-                                      items: [
-                                        {
-                                          id: "id-1",
-                                          name: "Rosie Simpson",
-                                          number: "459-12-56",
-                                        },
-                                        {
-                                          id: "id-2",
-                                          name: "Hermione Kline",
-                                          number: "443-89-12",
-                                        },
-                                        {
-                                          id: "id-3",
-                                          name: "Eden Clements",
-                                          number: "645-17-79",
-                                        },
-                                        {
-                                          id: "id-4",
-                                          name: "Annie Copeland",
-                                          number: "227-91-26",
-                                        },
-                                      ],
-                                    },
-                                    reducers: {
-                                      addContact: {
-                                        reducer(state,
-                                                action) {
-                                          state.items.push(action.payload);
-                                        },
-                                        prepare(name,
-                                                number) {
-                                          return {
-                                            payload: {
-                                              id: `id-${nanoid()}`,
-                                              name,
-                                              number,
-                                            },
-                                          };
-                                        },
-                                      },
-                                      deleteContact(state,
-                                                    action) {
-                                        state.items = state.items.filter(contact => contact.id !== action.payload);
-                                      },
-                                    },
-                                  });
+export const fetchContacts = createAsyncThunk(
+    "contacts/fetchAll",
+    async() => {
+      try {
+        const response = await axios.get('/contacts')
+        return response.data
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+      }
+    })
 
-export const {
-  addContact,
-  deleteContact,
-} = contactsSlice.actions;
+export const addContact = createAsyncThunk(
+    "contacts/addContact",
+    async(contact, thunkAPI) => {
+      try {
+        const response = await axios.get('/contacts', contact);
+        return response.data
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    })
 
-export const selectContacts = state => state.contacts.items;
+export const deleteContact = createAsyncThunk(
+    "contacts/deleteContact",
+    async(id, thunkAPI) => {
+      try {
+        const response = await axios.delete(`/contacts/${id}`);
+        return response.data;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    })
 
-export default contactsSlice.reducer;
+const initialState = {
+  items: [],
+  loading: false,
+  error: null,
+}
+
+export const contactsSlice = createSlice({
+  name: "contacts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+        .addCase(fetchContacts.pending, state => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(fetchContacts.fulfilled, (state, action) => {
+          state.loading = false;
+          state.items = action.payload;
+        })
+        .addCase(fetchContacts.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+        })
+        .addCase(addContact.pending, state => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(addContact.fulfilled, (state, action) => {
+          state.loading = false;
+          state.items.push(action.payload);
+        })
+        .addCase(addContact.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+        })
+        .addCase(deleteContact.pending, state => {
+          state.loading = true;
+          state.error = null;
+        })
+        .addCase(deleteContact.fulfilled, (state, action) => {
+          state.loading = false;
+          state.error = state.items.filter(contact => contact.id !== action.payload);
+        })
+        .addCase(deleteContact.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+        })
+  }
+})
+
+export const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
-
-import storage from "redux-persist/lib/storage";
-import contactsReducer from "./contactsSlice.js";
-import filtersReducer from "./filtersSlice.js";
-
-const persistConfig = {
-  key: "contacts",
-  storage,
-  whitelist: ["items"],
-};
-
-const persistedContactReducer = persistReducer(persistConfig,
-                                               contactsReducer);
 
 const store = configureStore({
-                               reducer: {
-                                 contacts: persistedContactReducer,
-                                 filters: filtersReducer,
-                               },
-                               middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-                             });
-
-export const persistor = persistStore(store);
+  reducer: {
+    contacts: contactsReducer,
+    filters: filterReducer
+  }
+});
 
 export default store;
