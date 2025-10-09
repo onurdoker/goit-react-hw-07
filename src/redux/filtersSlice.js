f16b4ac23ff3b3db57378659bff556ca636b8b98
Onur DÖKER <odoker@msn.com>
2025-10-09 23:24:12 +0300

    Update store configuration and refactor filters slice

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,20 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const filtersSlice = createSlice({
-                                   name: "filters",
-                                   initialState: {
-                                     name: "",
-                                   },
-                                   reducers: {
-                                     changeFilter(state,
-                                                  action) {
-                                       state.name = action.payload;
-                                     },
-                                   },
-                                 });
+const initialState = {
+  name: "",
+};
 
+export const filtersSlice = createSlice({
+                                          name: "filters",
+                                          initialState,
+                                          reducers: {
+                                            changeFilter: (state,
+                                                           action) => {
+                                              state.name = action.payload;
+                                            },
+                                          },
+                                        });
+
+export const filtersReducer = filtersSlice.reducer;
 export const { changeFilter } = filtersSlice.actions;
 
 export const selectNameFilter = state => state.filters.name;
-
-export default filtersSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "./contactsSlice.js";
+import filtersReducer from "./filtersSlice.js";
 
 const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filterReducer
-  }
-});
+                               reducer: {
+                                 contacts: contactsReducer,
+                                 filters: filtersReducer,
+                               },
+                             });
 
 export default store;
